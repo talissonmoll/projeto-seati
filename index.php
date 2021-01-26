6d144d9c0628588923ae0c81aba928e0d977bff3
Talisson Moll <talissonmoll@gmail.com>
2021-01-26 19:29:17 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="soluções, seati, segurança, eletrônica, eletronica, seguranca, eletrica, serralheria, construção civil">
+    <meta name="description" content="SEATI Soluções é referência nos serviços de Segurança Eletrônica, Elétrica, Refrigeração, Construção Civil e Serralheria">
+    
+    <title>SEATI SOLUÇÕES</title>
+</head>
+<body>
+    <header>
+        <div class="center">
+             <div class="logo">Logomarca</div><!--logo-->
+             <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Quem Somos</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>  
+            </nav>
+
+            <nav class="mobile>">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Quem Somos</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>  
+            </nav>
+
+        </div><!--center-->
+    </header>
+
+
+    <main>
+        <section class="banner-principal"><!--COLOCAR IMAGEM BANNER-->
+
+        <section class="descricao-empresa">
+            <h2>SEATI - SOLUÇÕES</h2>
+            <hr></hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim at impedit, aperiam ea nemo a consectetur odio voluptates est temporibus veritatis maiores nihil, aliquam eaque labore aspernatur omnis quos dicta?</p>
+            <p> <a href="" title = "Sobre SEATI"> "Saiba mais sobre a SEATI "</a> </p><!--BOTÃO SAIBA MAIS QUEM SOMOS NÓS-->
+
+        </section><!--descrilção-empresa-->
+
+        <div class="bg-index">
+        <div class ="wrapper">
+            <section class="servicos-index">
+                <h2>Nossos Serviços</h2>
+                    <div class="col-4">
+                            <div class="thumb">
+                             <img src="images/img-index1.jpg" alt="Manutenção Predial Civil" title="Manutenção Predial Civil">
+                             <h1 class="white txtcenter">Manutenção Predial Civil</h1>
+                             <hr>
+                             <p class="white font txtcenter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
+                             </p>
+                             <a href="">Saiba mais</a>
+
+                            </div>
+                    </div>
+                    <div class="col-4">
+                            <div class="thumb">
+                             <img src="images/img-index2.png" alt="Segurança Eletrônica" title="Segurança Eletrônica">
+                             <h1 class="white txtcenter">Segurança Eletrônica</h1>
+                             <hr>
+                             <p class="white font txtcenter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
+                             </p>
+                             <a href="">Saiba mais</a>                 
+                             </div>
+                     </div>    
+                     <div class="col-4">
+                            <div class="thumb">
+                             <img src="images/img-index3.jpg" alt="Preventiva Ar Condicionado" title="Preventiva Ar Condicionado">
+                             <h1 class="white txtcenter">Preventiva em Ar Condicionado</h1>
+                             <hr>
+                             <p class="white font txtcenter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
+                             </p>
+                             <a href="">Saiba mais</a>
+
+                            </div>
+                    </div>
+            </section> <!--serviços-index-->
+        </div><!-- Wrapper -->
+        </div><!-- bg-index -->
+    </main>
+
+    
+    <footer>
+    RODAPÉ
+    </footer>
+
+    
+</body>
+</html>
