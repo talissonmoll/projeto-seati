fb4748ba36aafddad47516b10014cc301625d399
Talisson Moll <talissonmoll@gmail.com>
2021-01-28 01:31:46 -0300

    28-01-21
    Protótipo pronto

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&display=swap" rel="stylesheet">
+    <link href="css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;800&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="soluções, seati, segurança, eletrônica, eletronica, seguranca, eletrica, serralheria, construção civil">
     <meta name="description" content="SEATI Soluções é referência nos serviços de Segurança Eletrônica, Elétrica, Refrigeração, Construção Civil e Serralheria">
@@ -24,6 +25,9 @@
             </nav>
 
             <nav class="mobile right>">
+                <div class="botao-menu-mobile">
+                <i class="fas fa-align-justify"></i>
+                </div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Quem Somos</a></li>
@@ -38,16 +42,16 @@
 
 
     <main>
-        <section class="banner-principal"></section><!--COLOCAR IMAGEM BANNER-->
+        
+        <section class="banner-principal center"></section><!--Banner Principal-->
+
 
         <section class="descricao-empresa">
             <div class="center">
-            <div class="w50">
-            <h1>SEATI - SOLUÇÕES</h1>
+            <h1 class="txtcenter">SEATI - SOLUÇÕES</h1>
             <hr></hr>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim at impedit, aperiam ea nemo a consectetur odio voluptates est temporibus veritatis maiores nihil, aliquam eaque labore aspernatur omnis quos dicta?</p>
-            <p> <a href="" title = "Sobre SEATI"> "Saiba mais sobre a SEATI "</a> </p><!--BOTÃO SAIBA MAIS QUEM SOMOS NÓS-->
-            </div><!--w50-->
+            <p class="txtcenter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim at impedit, aperiam ea nemo a consectetur odio voluptates est temporibus veritatis maiores nihil, aliquam eaque labore aspernatur omnis quos dicta?</p>
+            <p class="txtcenter"> <a href="" title = "Sobre SEATI" class="btn"> Saiba mais sobre a SEATI</a> </p><!--BOTÃO SAIBA MAIS QUEM SOMOS NÓS-->
             </div><!--center-->
         </section><!--descrilção-empresa-->
        
@@ -56,35 +60,35 @@
         <div class ="wrapper">
             <section class="servicos-index">
                 <h2>Nossos Serviços</h2>
-                    <div class="w33 left">
+                    <div class="w33 left row">
                             <div class="thumb">
                              <img src="images/img-index1.jpg" alt="Manutenção Predial Civil" title="Manutenção Predial Civil">
                              <h2 class="white txtcenter">Manutenção Predial Civil</h1>
                              <hr>
                              <p class="white font-sm txtcenter gap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
                              </p>
-                             <a href="">Saiba mais</a>
+                             <a class="btn" href="">Saiba mais</a>
 
                             </div>
                     </div>
-                    <div class="w33 left">
+                    <div class="w33 left row">
                             <div class="thumb">
                              <img src="images/img-index2.png" alt="Segurança Eletrônica" title="Segurança Eletrônica">
                              <h2 class="white txtcenter">Segurança Eletrônica</h1>
                              <hr>
                              <p class="white font-sm txtcenter gap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
                              </p>
-                             <a href="">Saiba mais</a>                 
+                             <a class="btn" href="">Saiba mais</a>                 
                              </div>
                      </div>    
-                     <div class="w33 left">
+                     <div class="w33 left row">
                             <div class="thumb">
                              <img src="images/img-index3.jpg" alt="Preventiva Ar Condicionado" title="Preventiva Ar Condicionado">
                              <h2 class="white txtcenter">Preventiva em Ar Condicionado</h1>
                              <hr>
                              <p class="white font-sm txtcenter gap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eveniet sapiente nemo provident quod facere nobis eos odit illum, porro nesciunt, excepturi recusandae nam, repellat sint sit saepe consectetur tempore!
                              </p>
-                             <a href="">Saiba mais</a>
+                             <a class="btn" href="">Saiba mais</a>
 
                             </div>
                     </div>
@@ -99,7 +103,7 @@
     
     <footer>
         <div class="center">
-        <p>SEATI Soluções - Todos os direitos reservados.</p>
+        <p>Copyright © - SEATI - Todos os direitos reservados.</p>
         </div><!--center-->
     </footer>
 
